9b5d08a96b565a0ac9d6f88cb61e0490d629213d
Lucamicheletti93 <luca.mike93@gmail.com>
2025-09-23 16:38:21 +0200

    Updating the directory

--- TestNonPromptCharmoniaMidAnchor22pass7/runme.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-export O2DPG_MC_CONFIG_ROOT=/cvmfs/alice.cern.ch/el9-x86_64/Packages/O2DPG/daily-20241114-0000-1  # version with the fix
-export ALIEN_JDL_O2DPG_MC_CONFIG_ROOT=/cvmfs/alice.cern.ch/el9-x86_64/Packages/O2DPG/daily-20241114-0000-1  # version with the fix
-export ALIEN_JDL_ANCHOR_SIM_OPTIONS="-gen external -ini $O2DPG_MC_CONFIG_ROOT/MC/config/PWGDQ/ini/GeneratorHF_bbbar_PsiAndJpsi_midy_triggerGap.ini"
-${O2DPG_ROOT}/MC/run/ANCHOR/anchorMC.sh
-
